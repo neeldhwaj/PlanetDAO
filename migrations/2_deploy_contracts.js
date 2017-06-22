8e5f5c94b8caf2789607e4a46f61f03d1e773858
meghanay <meghana.yadagiri@rhythmone.com>
2017-06-22 18:07:15 +0530

    add initial test cases

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var MetaCoin = artifacts.require("./MetaCoin.sol");'
+var Organisation = artifacts.require("./Organisation.sol");
+
 
 module.exports = function(deployer) {
+    deployer.deploy(Organisation);
 };
